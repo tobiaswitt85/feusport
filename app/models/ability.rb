7eeb484ad2c1dc8cf41e368492d0ac17deff2b25
Georg Limbach <georg.limbach@lichtbit.com>
2024-04-22 13:21:27 +0200

    Startseite gestalten

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    global_abilities
+
     return if user.nil?
 
     can(:manage, Competition)
     can(:manage, Document)
+  end
+
+  def global_abilities
+    can(:read, Competition, visible: true)
   end
 end
