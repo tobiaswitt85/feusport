a439050db025048a455845f103372d82839ab460
Georg Limbach <georg.limbach@lichtbit.com>
2024-04-25 14:33:51 +0200

    Disziplinen erweitern

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -2,6 +2,24 @@
 
 class Discipline < ApplicationRecord
   DISCIPLINES = %w[la hl hb zk gs fs other].freeze
+  DEFAULT_NAMES = {
+    la: 'Löschangriff nass',
+    hl: 'Hakenleitersteigen',
+    hb: '100m-Hindernisbahn',
+    zk: 'Zweikampf',
+    gs: 'Gruppenstafette',
+    fs: '4x100m-Feuerwehrstafette',
+    other: 'Andere',
+  }.with_indifferent_access.freeze
+  DEFAULT_SINGLE_DISCIPLINES = {
+    la: false,
+    hl: true,
+    hb: true,
+    zk: true,
+    gs: false,
+    fs: false,
+    other: false,
+  }.with_indifferent_access.freeze
 
   belongs_to :competition
   has_many :assessments, dependent: :restrict_with_error
