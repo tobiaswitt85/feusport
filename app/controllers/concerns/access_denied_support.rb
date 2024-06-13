ab13d2c5859ca5bb8fddf90fa2585b8093a1d4f5
Georg Limbach <georg.limbach@lichtbit.com>
2024-06-13 12:36:18 +0200

    Devise-Views anpassen

--- app/controllers/concerns/access_denied_support.rb
+++ app/controllers/concerns/access_denied_support.rb
@@ -14,9 +14,12 @@ module AccessDeniedSupport
         flash[:alert] = 'Zugriff verweigert'
         redirect_to root_path
       else
-        flash[:alert] = 'Bitte melden Sie sich an'
+        flash[:alert] = 'Bitte melden Dich an, um diese Funktion nutzen zu können.'
         session[:requested_url_before_login] = request.fullpath if request.format == :html
-        redirect_to new_user_session_path
+
+        redirect_params = {}
+        redirect_params[:info_hint] = :competition if exception.action == :new && exception.subject.is_a?(Competition)
+        redirect_to new_user_session_path(redirect_params)
       end
     end
   end
