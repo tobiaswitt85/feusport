30412997c01fdcb5438bb1a878b6deab4d64879a
Georg Limbach <georg.limbach@lichtbit.com>
2024-07-02 23:05:54 +0200

    JS reparieren

--- app/javascript/_edit_assessment_requests.js
+++ app/javascript/_edit_assessment_requests.js
@@ -8,26 +8,28 @@ const resetHandler = function () {
     checkbox.dispatchEvent(new Event('change'));
 
     const select = context.querySelector('select.assessment-types');
-    select.addEventListener('change', function () {
-      if (select.value === 'group_competitor') {
-        context.querySelector('.group-competitor-order').classList.remove('d-none');
-        context.querySelector('.single-competitor-order').classList.add('d-none');
-        context.querySelector('.competitor-order').classList.add('d-none');
-      } else if (select.value === 'single_competitor') {
-        context.querySelector('.single-competitor-order').classList.remove('d-none');
-        context.querySelector('.group-competitor-order').classList.add('d-none');
-        context.querySelector('.competitor-order').classList.add('d-none');
-      } else if (select.value === 'competitor') {
-        context.querySelector('.competitor-order').classList.remove('d-none');
-        context.querySelector('.group-competitor-order').classList.add('d-none');
-        context.querySelector('.single-competitor-order').classList.add('d-none');
-      } else {
-        context.querySelector('.single-competitor-order').classList.add('d-none');
-        context.querySelector('.group-competitor-order').classList.add('d-none');
-        context.querySelector('.competitor-order').classList.add('d-none');
-      }
-    });
-    select.dispatchEvent(new Event('change'));
+    if (select) {
+      select.addEventListener('change', function () {
+        if (select.value === 'group_competitor') {
+          context.querySelector('.group-competitor-order').classList.remove('d-none');
+          context.querySelector('.single-competitor-order').classList.add('d-none');
+          context.querySelector('.competitor-order').classList.add('d-none');
+        } else if (select.value === 'single_competitor') {
+          context.querySelector('.single-competitor-order').classList.remove('d-none');
+          context.querySelector('.group-competitor-order').classList.add('d-none');
+          context.querySelector('.competitor-order').classList.add('d-none');
+        } else if (select.value === 'competitor') {
+          context.querySelector('.competitor-order').classList.remove('d-none');
+          context.querySelector('.group-competitor-order').classList.add('d-none');
+          context.querySelector('.single-competitor-order').classList.add('d-none');
+        } else {
+          context.querySelector('.single-competitor-order').classList.add('d-none');
+          context.querySelector('.group-competitor-order').classList.add('d-none');
+          context.querySelector('.competitor-order').classList.add('d-none');
+        }
+      });
+      select.dispatchEvent(new Event('change'));
+    }
   });
 };
 
