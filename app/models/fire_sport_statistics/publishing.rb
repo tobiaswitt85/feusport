e7f5a1b56be938a9cfb9033836b348c8267b027d
Georg Limbach <georg.limbach@lichtbit.com>
2024-11-06 10:54:37 +0100

    require hinzufügen

--- app/models/fire_sport_statistics/publishing.rb
+++ app/models/fire_sport_statistics/publishing.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'net/http'
+
 class FireSportStatistics::Publishing < ApplicationRecord
   belongs_to :competition, class_name: '::Competition'
   belongs_to :user
